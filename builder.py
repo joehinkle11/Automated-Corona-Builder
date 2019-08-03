aaadcfb80c14b12a5ab7926c6534162b084fa1a3
Joe Hinkle <joehinkle11@gmail.com>
2019-08-03 17:19:49 -0400

    useful messages outputted

--- builder.py
+++ builder.py
@@ -34,13 +34,31 @@ def getKeyFromBuildSettings(key):
     else:
         raise Exception('There is no "' + key + '" in builder-settings.json')
 
+#
+# startup
+#
+ios_appName = getKeyFromBuildSettings('ios_appName')
+android_appName = getKeyFromBuildSettings('android_appName')
+
+print("-----------------------------------------------")
+print("----***************************************----")
+print("\tWelcome to Automated Corona Builder ")
+print("----***************************************----")
+print("-----------------------------------------------\n")
+
 
 #
 # 1. Determine whether we are building for iOS, Android, or both
 #
 
 platformsToBuildTo = getKeyFromBuildSettings('platformsToBuildTo')
-print(platformsToBuildTo)
+
+print(" Automated Corona Builder will build:")
+if 'ios' in platformsToBuildTo:
+    print(" - " + ios_appName+ " for iOS")
+if 'android' in platformsToBuildTo:
+    print(" - " + android_appName+ " for Android")
+print("\n")
 
 #
 # 2. Clean the `build_arguments/` and `builds/` folder
