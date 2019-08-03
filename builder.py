d432b4953d7d2fa6409d261a3b3bbe0d3b7273d0
Joe Hinkle <joehinkle11@gmail.com>
2019-08-03 17:04:43 -0400

    setting up more of builder.py

--- builder.py
+++ builder.py
@@ -1,11 +1,64 @@
+#
 # imports
+#
+
 import os
 import json
 from pythonThemerHelpers.makeFileChanges import makeFileChanges
 from pathlib import Path
 import shutil, errno
 
+#
 # constants
-coronaBuilderPath = '/Applications/CoronaSDK/Native/Corona/mac/bin/CoronaBuilder.app/Contents/MacOS/'
-themeVersioningPath = 'builder-settings.json'
-releaseLevelPriority = ["production","beta","alpha","internal"] #TODO
+#
+
+automatedBuildSettings = 'builder-settings.json'
+
+#
+# helper functions
+#
+
+def getKeyFromBuildSettings(key):
+    data = {}
+    try:
+        with open(automatedBuildSettings) as file:
+            data = json.loads(file.read())
+    except Exception as e:
+        pass
+
+    if key in data:
+        return str(data[key])
+    else:
+        raise Exception('There is no "' + key + '" in builder-settings.json')
+
+
+#
+# 1. Determine whether we are building for iOS, Android, or both
+#
+
+platformsToBuildTo = getKeyFromBuildSettings('platformsToBuildTo') #['ios','android'] TODO
+
+#
+# 2. Clean the `build_arguments/` and `builds/` folder
+#
+
+#
+# 3. Create build arguments for each build
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
